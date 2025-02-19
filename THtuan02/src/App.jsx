de5652503433d070ad57f6280f64fcce2dbd3839
H82M19\Student <Student>
2025-02-19 10:56:26 +0700

    Cau 2 Tuan 02

--- THtuan02/src/App.jsx
+++ THtuan02/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // Bai 1
   const[name,SetName] = useState('')
   const[result,SetResult] = useState('')
 
@@ -14,6 +15,19 @@ function App() {
     SetResult('hello:'+name)
   }
 
+  // Bai 2
+  const[A,SetA] = useState('')
+  const[B,SetB] = useState('')
+  const[result1,SetResult1] = useState('')
+  function handelChangeA(e){
+    SetA(e.target.value)
+  }
+  function handelChangeB(e){
+    SetB(e.target.value)
+  }
+  function handelChangeClickBai2(){
+    SetResult1((parseInt(A)+parseInt(B)))
+  }
 
   return (
     <>
@@ -21,9 +35,20 @@ function App() {
       <h1>Bai 1</h1>
       <input type="text" onChange={handelChangeName} placeholder='Nhap ten' />
       <br />
+      <br />
       <button onClick={handelChangeClick}>Click</button>
       <br />
       <span>{result}</span>
+    </div>
+
+    <div>
+      <h1>Bai 2</h1>
+      <input type="text" onChange={handelChangeA} placeholder='Nhap so A' />
+      <input type="text" onChange={handelChangeB} placeholder='Nhap so B' />
+      <br />
+      <button onClick={handelChangeClickBai2}>Click</button>
+      <br />
+      <span>{result1}</span>
     </div>
       
     </>
