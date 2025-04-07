475112a6efc8f297ae4ed64e1a2ceb9e8cb5622a
Quyen2111 <22640121.quyen@student.iuh.edu.vn>
2025-04-07 23:01:51 +0700

    complete 5. react router

--- tuan07/src/App.jsx
+++ tuan07/src/App.jsx
@@ -1,15 +1,18 @@
 
 import './App.css'
 import { Layout } from './component/layout'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { CustomDataTable } from './component/CustomDataTable'; 
 
 function App() {
-
-
   return (
-    <>
-    <Layout/>
-     
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/customdatapage" element={<CustomDataTable />} />
+      </Routes>
+    </Router>
   )
 }
 
